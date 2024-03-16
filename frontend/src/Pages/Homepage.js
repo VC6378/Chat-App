a9e7f6a79d76bdcc3405825dfb22cbe4bc0e3ad6
Vibhuti <vibhuti6378@gmail.com>
2024-03-16 17:38:08 +0530

    changes made

--- frontend/src/Pages/Homepage.js
+++ frontend/src/Pages/Homepage.js
@@ -1,8 +1,38 @@
-import React from 'react'
+import React from 'react';
+import { Container, Text, Box, Tabs, TabList, Tab, TabPanel, TabPanels, } from '@chakra-ui/react';
+import Login from '../components/Authentication/Login';
+import Signup from '../components/Authentication/Signup';
 
 const Homepage = () => {
     return (
-        <div>Homepage</div>
+        <Container maxW='xl' centerContent>
+            <Box
+                display="flex"
+                justifyContent="center"
+                p={3}
+                bg={"white"}
+                w="100%"
+                m="40px 0 15px 0"
+                borderRadius="1g"
+                borderWidth="1px"
+            ><Text fontSize="4xl" fontFamily="Work sans" color="black" fontWeight="300">Talk-A-Tive</Text></Box>
+            <Box bg="white" w="100%" p={4} borderRadius="1g" borderWidth="1px">
+                <Tabs variant='soft-rounded' colorScheme='blue'>
+                    <TabList mb="1em">
+                        <Tab width="50%">Login</Tab>
+                        <Tab width="50%">Sign Up</Tab>
+                    </TabList>
+                    <TabPanels>
+                        <TabPanel>
+                            <Login />
+                        </TabPanel>
+                        <TabPanel>
+                            <Signup/>
+                        </TabPanel>
+                    </TabPanels>
+                </Tabs>
+            </Box>
+        </Container >
     )
 }
 
